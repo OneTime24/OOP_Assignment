3cd883a8daab428a8bb8b94353900da93282abf5
OneTime24 <onetimehexa@gmail.com>
2026-03-04 05:43:14 +0500

    main branch

---
+++ task2.cpp
@@ -0,0 +1,12 @@
+
+// TASK 2: The Game Inventory That Eats Memory
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"THE GAME INVENTORY THAT EATS MEMORY:: MAIN BRANCH: "<<endl;
+
+
+}
